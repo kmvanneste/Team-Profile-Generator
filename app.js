5cf7fae2171122268e73ade7870f00024e68d21c
kmvanneste <kmvanneste17@gmail.com>
2020-08-17 23:45:03 -0400

    got the check email to validate user input

--- app.js
+++ app.js
@@ -5,84 +5,13 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const validator = require("email-validator");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputFile = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const team = [];
-
-function askQuestions() {
-    inquirer.prompt([
-        {
-            type: "list",
-            choices: ["Manager", "Engineer", "Intern"],
-            message:"What is your role in the company?",
-            name: "role",
-        },
-        {
-            type: "input",
-            message:"What is your full name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message:"What is your ID number?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message:"What is your email address?",
-            name: "email",
-        },
-    ]).then(function(response){
-        this.name = response.name;
-        this.id = response.id;
-        this.email = response.email;
-        this.role = response.role;
-        if (this.role === "Manager") {
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "What is your office number?",
-                    name: "officeNumber"
-                }
-            ]).then(function(response){
-                this.officeNumber = response.officeNumber;
-                team.push(new Manager(this.name, this.id, this.email, this.officeNumber));
-                console.log(new Manager(this.name, this.id, this.email, this.officeNumber));
-                return addMore();
-            })
-        } else if (this.role === "Engineer") {
-            inquirer.prompt([ 
-                {
-                    type: "input",
-                    message: "What is your GitHub?",
-                    name: "github"
-                }
-            ]).then(function(response) {
-                this.github = response.github;
-                team.push(new Engineer(this.name, this.id, this.email, this.github));
-                console.log(new Engineer(this.name, this.id, this.email, this.github));
-                return addMore();
-            })
-        } else if (this.role === "Intern") {
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "What is the name of your school?",
-                    name: "school"
-                }
-            ]).then(function(response) {
-                this.school = response.school;
-                team.push(new Intern(this.name, this.id, this.email, this.school));
-                console.log(new Intern(this.name, this.id, this.email, this.school));
-                return addMore();
-            })
-        }
-    })
-}
-askQuestions();
 
 function addMore() {
     inquirer.prompt([
@@ -112,14 +41,104 @@ function addMore() {
     });
 }
 
+function askQuestions() {
+    inquirer.prompt([
+        {
+            type: "list",
+            choices: ["Manager", "Engineer", "Intern"],
+            message:"What is your role in the company?",
+            name: "role",
+        },
+        {
+            type: "input",
+            message:"What is your full name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message:"What is your ID number?",
+            name: "id",
+        }
+    ]).then(function(response){
+        this.name = response.name;
+        this.id = response.id;
+        this.role = response.role;
+        
+        function checkEmail() {
+            inquirer.prompt([
+                {
+                    type: "email",
+                    message:"What is your email address?",
+                    name: "email",
+                }
+             ]).then(function(response){
+                this.email = response.email;
+                if (validator.validate(this.email)) {
+                    this.email = response.email;
+                    
+                                    if (this.role === "Manager") {
+                                        inquirer.prompt([
+                                            {
+                                                type: "input",
+                                                message: "What is your office number?",
+                                                name: "officeNumber"
+                                            }
+                                        ]).then(function(response){
+                                            this.officeNumber = response.officeNumber;
+                                            team.push(new Manager(this.name, this.id, this.email, this.officeNumber));
+                                            console.log(new Manager(this.name, this.id, this.email, this.officeNumber));
+                                            return addMore();
+                                        })
+                                    } else if (this.role === "Engineer") {
+                                        inquirer.prompt([ 
+                                            {
+                                                type: "input",
+                                                message: "What is your GitHub?",
+                                                name: "github"
+                                            }
+                                        ]).then(function(response) {
+                                            this.github = response.github;
+                                            team.push(new Engineer(this.name, this.id, this.email, this.github));
+                                            console.log(new Engineer(this.name, this.id, this.email, this.github));
+                                            return addMore();
+                                        })
+                                    } else if (this.role === "Intern") {
+                                        inquirer.prompt([
+                                            {
+                                                type: "input",
+                                                message: "What is the name of your school?",
+                                                name: "school"
+                                            }
+                                        ]).then(function(response) {
+                                            this.school = response.school;
+                                            team.push(new Intern(this.name, this.id, this.email, this.school));
+                                            console.log(new Intern(this.name, this.id, this.email, this.school));
+                                            return addMore();
+                                        })
+                                    }
+                                 } else {
+                                        console.log("Please enter a valid email address.");
+                                        checkEmail();
+                                }
+                            })          
+                            
+                        }
+         checkEmail();
+    })
+}
+
+askQuestions();
 
 
-//FUNCTION CHECKING TO SEE IF USER INPUTS A PROPER EMAIL ADDRESS    
+
+
+
+// FUNCTION CHECKING TO SEE IF USER INPUTS A PROPER EMAIL ADDRESS    
     // const emailSymbol = "@";
     // function checkEmail() {
     //     let email = this.email.split("");
     //     if (email.includes(emailSymbol)) {
-    //         return;
+    //         this.email = email;
     //     } else {
     //         console.log("Please enter a valid email address.");
     //         return inquirer.prompt([
@@ -134,4 +153,4 @@ function addMore() {
     //     }
     // }
     // checkEmail();
-//-------------------------------------------------------------------------- 
+// -------------------------------------------------------------------------- 
